c9fa430387d8be6fd01615e7bb6ab27037888368
Nanthawat45 <Nanthawat.Ohm45@gmail.com>
2024-06-23 22:27:03 +0700

    first commit

---
+++ src/components/Card2.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+const Card2 = () => {
+ 
+  return (
+    <div> 
+
+      <div className="flex flex-wrap justify-center space-x-4 space-y-4">
+  <div className="card card-compact w-full sm:w-60 bg-base-100 shadow-xl">
+    <figure>
+      <img src="https://food-cms.grab.com/compressed_webp/items/THITE2020032412195449051/detail/menueditor_item_654a1e9ba95c45b08d6e02cb3d125564_1690190566168200391.webp" alt="Shoes" />
+    </figure>
+    <div className="card-body">
+      <h2 className="card-title">ข้าวมันไก่ต้ม</h2>
+      <p>โปรดระบุ หากต้องการ ซีอิ๊วดำ, พริก, ขิง. เสิร์ฟพร้อมน้ำจิ้มและน้ำซุป Please specify if need sweet soy sauce, chilli, ginger. Served with sauce and soup</p>
+      <div className="card-actions justify-end">
+        <button className="btn btn-primary">65</button>
+      </div>
+    </div>
+  </div>
+
+  <div className="card card-compact w-full sm:w-60 bg-base-100 shadow-xl">
+    <figure>
+      <img src="https://food-cms.grab.com/compressed_webp/items/THITE2020032412175815333/detail/menueditor_item_99d5cc6ae88546a5a7d29a09514a9b8e_1690191016775341840.webp" alt="Shoes" />
+    </figure>
+    <div className="card-body">
+      <h2 className="card-title">ข้าวมันไก่ทอด</h2>
+      <p>โปรดระบุ หากต้องการ ซีอิ๊วดำ, พริก, ขิง. เสิร์ฟพร้อมน้ำจิ้มและน้ำซุป Please specify if need sweet soy sauce, chilli, ginger. Served with sauce and soup</p>
+      <div className="card-actions justify-end">
+        <button className="btn btn-primary">65</button>
+      </div>
+    </div>
+  </div>
+
+  <div className="card card-compact w-full sm:w-60 bg-base-100 shadow-xl">
+    <figure>
+      <img src="https://food-cms.grab.com/compressed_webp/items/THITE2020032412214922633/detail/menueditor_item_5162539a4894431892d9429d417bc890_1690190826793908408.webp" alt="Shoes" />
+    </figure>
+    <div className="card-body">
+      <h2 className="card-title">ข้าวมันไก่ ผสม ต้มทอด</h2>
+      <p>โปรดระบุ หากต้องการ ซีอิ๊วดำ, พริก, ขิง. เสิร์ฟพร้อมน้ำจิ้มและน้ำซุป Please specify if need sweet soy sauce, chilli, ginger. Served with sauce and soup</p>
+      <div className="card-actions justify-end">
+        <button className="btn btn-primary">80</button>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+  
+
+
+
+
+
+
+    </div>
+  );
+}
+
+export default Card2
---
+++ src/components/Restaurant.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './Card'
+
+const Restaurant = () => {
+    // const resto ={
+    //     // img:"",
+    //     // title:""
+    // }
+  return (
+    <div>Restaurant</div>
+  )
+}
+
+export default Restaurant
